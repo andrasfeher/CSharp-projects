d4afb4288893ddc9d13090de5f60c0be1bf877af
andrasfeher <andras.feher@gmail.com>
2016-09-14 17:18:32 +0200

    Add files via upload

---
+++ Defect/Entities/Defect.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Xml.Serialization;
+using System.Threading;
+
+namespace WebService1
+{
+    [XmlInclude(typeof(DefectState))]
+    public class Defect
+    {
+        int _id;
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+        string _description;
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        string _createdBy;
+        public string CreatedBy
+        {
+            get { return _createdBy; }
+            set { _createdBy = value; }
+        }
+
+        DefectState _state;
+        public DefectState State
+        {
+            get { return _state; }
+            set { _state = value; }
+        }
+
+        DateTime? _createdDate;
+        public DateTime? CreatedDate
+        {
+            get { return _createdDate; }
+            set { _createdDate = value; }
+        }
+
+        string _assignedTo;
+        public string AssignedTo
+        {
+            get { return _assignedTo; }
+            set { _assignedTo = value; }
+        }
+
+        DateTime? _assignedDate;
+        public DateTime? AssignedDate
+        {
+            get { return _assignedDate; }
+            set { _assignedDate = value; }
+        }
+
+        DateTime? _closedDate;
+        public DateTime? ClosedDate
+        {
+            get { return _closedDate; }
+            set { _closedDate = value; }
+        }
+
+        private byte[] _attachedFile;
+
+        public byte[] AttachedFile
+        {
+            get { return _attachedFile; }
+            set { _attachedFile = value; }
+        }
+
+
+        public Defect()
+        {
+        }
+
+        private bool IsNewStateAndRoleCompatible(DefectState newState )
+        {
+            bool result = false;
+
+            switch (newState)
+            {
+                case DefectState.Opened:
+                case DefectState.Closed: if (Thread.CurrentPrincipal.IsInRole("tesztelő"))
+                                result = true;
+                             break;
+                case DefectState.UnderDevelopement:
+                case DefectState.Resolved:
+                case DefectState.Rejected: if (Thread.CurrentPrincipal.IsInRole("fejlesztő"))
+                                 result = true;
+                               break;
+                default:
+                    result = false;
+                    break;
+            }
+
+            return result;
+        }
+
+
+        private bool IsOldStateAndNewStateCompatible(DefectState newState)
+        {
+            bool result = false;
+
+            switch (State)
+            {
+                case DefectState.Opened: if (newState == DefectState.UnderDevelopement)
+                                result = true;
+                             break;
+                case DefectState.UnderDevelopement: if ( newState == DefectState.Resolved 
+                                                         || newState == DefectState.Rejected
+                                                       )
+                                 result = true;
+                             break;
+                case DefectState.Resolved:
+                case DefectState.Rejected: if (newState == DefectState.Closed)
+                                 result = true;
+                             break;
+                default:
+                    result = false;
+                    break;
+            }
+
+            return result;
+        }
+
+        public void SetState(DefectState newState)
+        {
+            if ( IsNewStateAndRoleCompatible(newState) )
+            {
+                if (IsOldStateAndNewStateCompatible(newState))
+                {
+                    State = newState;
+                }
+                else
+                {
+                    throw new ApplicationException("Invalid state change!");
+                }
+            }
+            else
+            {
+                throw new ApplicationException("The new state and the user role are incompatible!");
+            }
+        }
+    }
+
+    public enum DefectState
+    {
+        Opened, UnderDevelopement, Resolved, Rejected, Closed
+    }
+
+
+}
---
+++ Defect/WebService1/Service1.asmx.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.Xml.Linq;
+using System.Collections.Generic;
+using System.Security.Principal;
+using System.Threading;
+using System.Web.Configuration;
+using System.Xml;
+
+namespace WebService1
+{
+    /// <summary>
+    /// Summary description for Service1
+    /// </summary>
+    [WebService(Namespace = "http://defects.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Service1 : System.Web.Services.WebService
+    {
+
+        string[] _roles;
+
+        public string[] Roles
+        {
+          get { return _roles; }
+          set { _roles = value; }
+        }
+
+        public Service1()
+        {
+            try
+            {
+                IIdentity current = HttpContext.Current == null ? Thread.CurrentPrincipal.Identity : HttpContext.Current.User.Identity;
+                string name = current.Name;
+                if (string.IsNullOrEmpty(name))
+                {
+                    throw new SoapException("Name is null or empty!", new XmlQualifiedName("MySoapException"));
+                }
+                string role = WebConfigurationManager.AppSettings[name];
+                if (string.IsNullOrEmpty(role))
+                {
+                    throw new SoapException("Role is null or empty!", new XmlQualifiedName("MySoapException"));
+                }
+                Roles = role.Split(',');
+                GenericPrincipal p = new GenericPrincipal(current, role.Split(','));
+                Thread.CurrentPrincipal = p;
+            }
+            catch (Exception ex)
+            {
+                throw new SoapException(ex.ToString(), new XmlQualifiedName("MySoapException"));
+            }
+        }
+
+        [WebMethod]
+        public string[] GetRoles()
+        {
+            try
+            {
+                return Roles;
+            }
+            catch (Exception ex)
+            {
+                throw new SoapException(ex.ToString(), new XmlQualifiedName("MySoapException"));
+            }
+        }
+
+        [WebMethod]
+        public Defect GetDefect(int Id)
+        {
+            try
+            {
+                DefectBusinessLogic.BusinessLogic bl = new DefectBusinessLogic.BusinessLogic();
+                return bl.GetDefect(Id);
+            }
+            catch (Exception ex)
+            {
+                throw new SoapException(ex.ToString(), new XmlQualifiedName("MySoapException"));
+            }
+        }
+
+        [WebMethod]
+        public Defect AddDefect(Defect item)
+        {
+            try
+            {
+                DefectBusinessLogic.BusinessLogic bl = new DefectBusinessLogic.BusinessLogic();
+                return bl.AddDefect(item);
+            }
+            catch (Exception ex)
+            {
+                throw new SoapException(ex.ToString(), new XmlQualifiedName("MySoapException"));
+            }
+        }
+
+        [WebMethod]
+        public List<Defect> GetDefects()
+        {
+            try
+            {
+                DefectBusinessLogic.BusinessLogic bl = new DefectBusinessLogic.BusinessLogic();
+                return bl.GetDefects();
+            }
+            catch (Exception ex)
+            {
+                throw new SoapException(ex.ToString(), new XmlQualifiedName("MySoapException"));
+            }
+        }
+
+        [WebMethod]
+        public Defect SetDefectState(Defect item, DefectState newState)
+        {
+            try
+            {
+                DefectBusinessLogic.BusinessLogic bl = new DefectBusinessLogic.BusinessLogic();
+                return bl.SetDefectState(item, newState);
+            }
+            catch (Exception ex)
+            {
+                throw new SoapException(ex.ToString(), new XmlQualifiedName("MySoapException"));
+            }
+        }
+    }
+}
